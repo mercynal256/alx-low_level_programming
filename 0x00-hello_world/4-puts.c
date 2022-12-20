8ef1138d657c1e77fbbf1fa64e93a7d19e741b39
mercynal256 <nalulemercy@ieee.org>
2022-12-20 13:53:52 -0800

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- *  * Description: main - Prints out a string to stdout.
- *   * Return: 0 if success.
- *    */
+ * main - Entry point
+ * return: Always 0 (Success)
+ */
 int main(void)
 {
-		puts("\"Programming is like building a multilingual puzzle");
-			return (0);
+	puts("\"Programmming is like building a mutilingual puzzle");
+	return (0);
 }
