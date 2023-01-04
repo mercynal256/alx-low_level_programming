8905ddd8ee79221744644ceee353c2d9f134dbe2
mercynal256 <nalulemercy@ieee.org>
2023-01-04 06:49:24 -0800

    task 0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * __puts_recursion - prints a string followed by a new line.
+ * _puts_recursion - prints a string followed by a new line.
  * @s: pointer to string.
  *
  * Return: void.
