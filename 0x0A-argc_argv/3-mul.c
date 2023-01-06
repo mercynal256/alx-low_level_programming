64d5a48e4910cbdaa87c87598625b06dbf0a47fa
mercynal256 <nalulemercy@ieee.org>
2023-01-06 08:34:18 -0800

    task 3

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -14,7 +14,6 @@ int main(int argc, char *argv[])
 	int index, multiplication;
 
 	multiplication = 1;
-
 	if (argc < 3)
 	{
 		printf("Error\n");
@@ -24,7 +23,6 @@ int main(int argc, char *argv[])
 	{
 		multiplication = multiplication * atoi(agrv[index]);
 	}
-
 	printf("%d\n", multiplication);
 	return (0);
 }
